0d311c41d6b54a2df2af27da8e0be4c305298d8c
Adam Allsebrook <adsorigin@gmail.com>
2023-03-21 22:32:00 +0000

    Add shots in box to SimpleShotData

--- client/src/lib/data/ShotData.ts
+++ client/src/lib/data/ShotData.ts
@@ -3,10 +3,19 @@ import type { Shot, SimpleShot } from '$client';
 
 export class SimpleShotData {
     shots: number
+    insideBox: number
+    outsideBox: number
     
     constructor(shots: SimpleShot[]) {
         this.shots = shots.length;
+        this.insideBox = shots.filter(SimpleShotData.isInBox).length;
+        this.outsideBox= this.shots - this.insideBox;
     }
+
+    private static isInBox(shot: SimpleShot) {
+        return shot.Y > 15/74 && shot.Y < 59/74 && shot.X > 97/115;
+    }
+}
 
 
 export class ShotData extends SimpleShotData {
