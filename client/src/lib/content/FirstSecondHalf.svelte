5d63b1bb2fd0c5ce6ddab1cf27d7c50fa78fc6cc
Adam Allsebrook <adsorigin@gmail.com>
2023-03-25 16:03:58 +0000

    Refactor FirstSecondHalf using dataManager

--- client/src/lib/content/FirstSecondHalf.svelte
+++ client/src/lib/content/FirstSecondHalf.svelte
@@ -1,24 +1,23 @@
 <script lang='ts'>
-    import type { Player, Match, Shot, Fixture, Team, SimpleShot } from '$client';
+    import type { SimpleShot } from '$client';
+    import { key as dataKey, type DataManager } from '$lib/data/dataManager';
+    import { getContext } from 'svelte';
 
-    export let player: Player;
-    export let shots: Shot[];
-    export let matches: Match[];
-    export let fixtures: Fixture[];
-    export let teams: Map<string, Team>;
     export let allShotsAgainstFixtures: SimpleShot[];
-
-    $: firstHalfShots = shots.filter(d => d.minute <= 45);
-    $: firstHalfPercent = firstHalfShots.length / shots.length * 100;
-    $: prefersFirstHalf = firstHalfPercent > 50;
+    const dataManager: DataManager = getContext(dataKey);
+    const player = dataManager.player;
+    const shotData = dataManager.shotData;
 
     $: fixturesFirstHalf = allShotsAgainstFixtures.filter(d => d.minute >= 45);
     $: fixturesFirstHalfPercent = fixturesFirstHalf.length / allShotsAgainstFixtures.length * 100;
+    const prefersFirstHalf = shotData.firstHalf > shotData.secondHalf;
+    const firstHalfPercent = shotData.strPercent(shotData.firstHalf);
+    const secondHalfPercent = shotData.strPercent(shotData.secondHalf);
 </script>
 
 <h3 class='font-bold text-2xl'>First or Second Half</h3>
 <p>
-    {player.player_name} has more luck in the {prefersFirstHalf ? 'first' : 'second'} half, with {Math.max(firstHalfPercent, 100 - firstHalfPercent).toFixed(2)}% of his shots.
+    {player.player_name} has more luck in the {prefersFirstHalf ? 'first' : 'second'} half, with {prefersFirstHalf ? firstHalfPercent : secondHalfPercent}% of his shots.
 </p>
 <p>
     {player.team_title}'s next opponents have conceded {prefersFirstHalf ? fixturesFirstHalfPercent.toFixed(2) : (100 - fixturesFirstHalfPercent).toFixed(2)}% of their shots in the {prefersFirstHalf ? 'first' : 'second'} half.
