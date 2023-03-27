c67a0740d69d72f3e57a6f0e4fc4396c2e373efe
Adam Allsebrook <adsorigin@gmail.com>
2023-03-27 23:13:34 +0100

    Refactor Intro to use dataManager

--- client/src/lib/content/Intro.svelte
+++ client/src/lib/content/Intro.svelte
@@ -1,27 +1,29 @@
-<script lang='ts'>
-    import type { Player, Match, Shot, Fixture, Team } from '$client';
-	import FixtureList from '$lib/FixtureList.svelte';
+<script lang="ts">
+    import { key as dataKey, type DataManager } from '$lib/data/dataManager';
+    import FixtureList from '$lib/FixtureList.svelte';
+    import { getContext } from 'svelte';
 
-    export let player: Player;
-    export let shots: Shot[];
-    export let matches: Match[];
-    export let fixtures: Fixture[];
-    export let teams: Map<string, Team>;
+    const dataManager: DataManager = getContext(dataKey);
+    const player = dataManager.player;
+    const shots = dataManager.shots;
+    const teams = dataManager.teams;
+    const matches = dataManager.matches;
+    const fixtures = dataManager.fixtures;
 
-    $: sumXG = shots.map(d => d.xG).reduce((x, y) => x+y, 0);
-    $: sumMins = matches.map(d => d.time).reduce((x, y) => x+y, 0)
+    const sumXG = dataManager.shotData.xG;
+    const sumMins = dataManager.matchData.minutes;
 </script>
 
-<h3 class='font-bold text-2xl mb-2'>Introduction</h3>
+<h3 class="font-bold text-2xl mb-2">Introduction</h3>
 <p>
-    {player.player_name} has taken {shots.length} shots this season, 
-    scoring {shots.filter(d => d.result === 'Goal').length} goals. 
+    {player.player_name} has taken {shots.length} shots this season, scoring
+    {shots.filter((d) => d.result === 'Goal').length} goals.
 </p>
 <p>
-    He has accumulated a total of {sumXG.toFixed(2)} xG over {matches.length} matches, or <i class='underline-thick decoration-[#00FF7Faa] font-extrabold'>{(sumXG / sumMins * 90).toFixed(2)}</i> xG90.
+    He has accumulated a total of {sumXG.toFixed(2)} xG over {matches.length} matches, or
+    <i class="underline-thick decoration-[#00FF7Faa] font-extrabold">
+        {((sumXG / sumMins) * 90).toFixed(2)}
+    </i> xG90.
 </p>
 
-<FixtureList
-    fixtures={fixtures.slice(0, 5)}
-    {teams}
-/>
+<FixtureList fixtures={fixtures.slice(0, 5)} {teams} />
