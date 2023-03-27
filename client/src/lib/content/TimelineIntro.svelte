85e3d477e7ccc3b54e15a887dfa1f3bbbe447024
Adam Allsebrook <adsorigin@gmail.com>
2023-03-27 23:34:26 +0100

    Refactor TimelineIntro to use dataManager

--- client/src/lib/content/TimelineIntro.svelte
+++ client/src/lib/content/TimelineIntro.svelte
@@ -1,7 +1,10 @@
 <script lang='ts'>
     import type { Player } from '$client';
+    import { getContext } from 'svelte';
+    import { type DataManager, key as dataKey } from '$lib/data/dataManager';
 
-    export let player: Player;
+    const dataManager: DataManager = getContext(dataKey);
+    const player: Player = dataManager.player;
 </script>
 
 <h3 class='font-bold text-2xl'>Timeline</h3>
