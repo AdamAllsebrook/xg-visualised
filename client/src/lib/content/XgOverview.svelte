d51af7c551ba532f853d16d368f35efadac27ca2
Adam Allsebrook <adsorigin@gmail.com>
2023-03-27 23:51:45 +0100

    Refactor XgOverview to use dataManager

--- client/src/lib/content/XgOverview.svelte
+++ client/src/lib/content/XgOverview.svelte
@@ -1,39 +1,36 @@
 <script lang='ts'>
-    import type { Player, Match, Shot, Fixture, Team } from '$client';
+    import { key as dataKey, type DataManager } from "$lib/data/dataManager";
+    import { getContext } from "svelte";
 
-    export let player: Player;
-    export let shots: Shot[];
-    export let matches: Match[];
-    export let fixtures: Fixture[];
-    export let teams: Map<string, Team>;
 
-    $: sumXG = shots.map(d => d.xG).reduce((x, y) => x+y, 0);
-    $: sumMins = matches.map(d => d.time).reduce((x, y) => x+y, 0)
-    $: goals = shots.filter(d => d.result === 'Goal'); 
-    $: nonPenalties = shots.filter(d => d.situation !== 'Penalty');
-    $: scoredPenalties = goals.filter(d => d.situation === 'Penalty');
-    $: sumNpXg = nonPenalties.map(d => d.xG).reduce((x, y) => x+y, 0);
-
-    function per90(n: number, mins: number) {
-        return (n / mins * 90).toFixed(2);
-    }
+    const dataManager: DataManager = getContext(dataKey);
+    const player = dataManager.player;
+    const shots = dataManager.shotData.shots;
+    const goals = dataManager.shotData.scored;
+    const sumXG = dataManager.shotData.xG;
+    const sumMins = dataManager.matchData.minutes;
+    const sumNpXg = dataManager.shotData.npxG;
+    const matches = dataManager.matchData.matches;
+    const penalties = dataManager.shotData.penalties;
+    const nonPenalties = shots - penalties;
+    const scoredPenalties = dataManager.shotData.penaltiesScored;
 </script>
 
 <p>
-    {player.player_name} has taken {shots.length} shots this season, 
-    scoring {goals.length} goals. 
+    {player.player_name} has taken {shots} shots this season, 
+    scoring {goals} goals. 
 </p>
 <p>
-    He has accumulated a total of {sumXG.toFixed(2)} xG over {matches.length} matches, or <i class='underline-thick decoration-[#00FF7Faa] font-extrabold'>{(sumXG / sumMins * 90).toFixed(2)}</i> xG90.
+    He has accumulated a total of {sumXG.toFixed(2)} xG over {matches} matches, or <i class='underline-thick decoration-[#00FF7Faa] font-extrabold'>{(sumXG / sumMins * 90).toFixed(2)}</i> xG90.
 </p>
 <p>
-    This means he is {goals.length > sumXG ? 'over' : 'under'}performing by {(Math.abs(goals.length - sumXG) / sumMins * 90).toFixed(2)} xG90.
+    This means he is {goals > sumXG ? 'over' : 'under'}performing by {(Math.abs(goals - sumXG) / sumMins * 90).toFixed(2)} xG90.
 </p>
 <br>
 <p>
-    {#if nonPenalties.length == shots.length}
+    {#if nonPenalties == shots}
         No shots taken have been penalties.
     {:else}
-        He has taken {shots.length - nonPenalties.length} penalt{shots.length - nonPenalties.length == 1 ? 'y' : 'ies'} (scoring {scoredPenalties.length}), resulting in a npXG90 of {per90(sumNpXg, sumMins)}.
+        He has taken {shots - nonPenalties} penalt{shots - nonPenalties == 1 ? 'y' : 'ies'} (scoring {scoredPenalties}), resulting in a npXG90 of {(sumNpXg / sumMins * 90).toFixed(2)}.
     {/if}
 </p>
