e8558fa463ed24966d02be381d7725d98dfe0f18
Adam Allsebrook <adsorigin@gmail.com>
2023-03-27 23:52:03 +0100

    Add matches property to MatchData

--- client/src/lib/data/matchData.ts
+++ client/src/lib/data/matchData.ts
@@ -2,9 +2,11 @@ import type { Match } from '$client';
 import { sum } from './utils';
 
 export class MatchData {
+    matches: number;
     minutes: number;
 
     constructor(matches: Match[]) {
+        this.matches = matches.length;
         this.minutes = matches.map((match) => match.time).reduce(sum);
     }
 }
