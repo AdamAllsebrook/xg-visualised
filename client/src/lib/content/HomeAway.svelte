800792ad2b8f729b85967d6149a8f4ea390cc5b9
Adam Allsebrook <adsorigin@gmail.com>
2023-03-27 22:57:59 +0100

    Refactor HomeAway to use dataManager

--- client/src/lib/content/HomeAway.svelte
+++ client/src/lib/content/HomeAway.svelte
@@ -1,24 +1,33 @@
-<script lang='ts'>
-    import type { Player, Match, Shot, Fixture, Team, SimpleShot } from '$client';
+<script lang="ts">
+    import type { SimpleShot } from '$client';
+    import type { DataManager } from '$lib/data/dataManager';
+    import { key as dataKey } from '$lib/hoveredData';
+    import { getContext } from 'svelte';
 
-    export let player: Player;
-    export let shots: Shot[];
-    export let matches: Match[];
-    export let fixtures: Fixture[];
-    export let teams: Map<string, Team>;
     export let allShotsAgainstFixtures: SimpleShot[];
+    const dataManager: DataManager = getContext(dataKey);
+    const player = dataManager.player;
+    const shotData = dataManager.shotData;
 
-    $: shotsHome = shots.filter(d => d.h_a === 'h');
-    $: prefersHome = shotsHome.length / shots.length > 0.5;
+    const shotsHome = shotData.home;
+    const shotsAway = shotData.away;
+    const prefersHome = shotsHome > shotsAway;
+    const shotsHomePercent = shotData.strPercent(shotsHome);
+    const shotsAwayPercent = shotData.strPercent(shotsAway);
 
     // h_a is reversed here as it refers to the player taking the shot
-    $: fixturesHome = allShotsAgainstFixtures.filter(d => d.h_a == 'a');
+    $: fixturesHome = allShotsAgainstFixtures.filter((d) => d.h_a == 'a');
 </script>
 
-<h3 class='font-bold text-2xl'>Home and Away</h3>
+<h3 class="font-bold text-2xl">Home and Away</h3>
 <p>
-    {player.player_name} has more shots in his {prefersHome ? 'home' : 'away'} games, with {(Math.max(shotsHome.length / shots.length, 1-shotsHome.length / shots.length) * 100).toFixed(2)}% of his shots.
+    {player.player_name} has more shots in his {prefersHome ? 'home' : 'away'} games, with {prefersHome
+        ? shotsHomePercent
+        : shotsAwayPercent}% of his shots.
 </p>
 <p>
-    {player.team_title}'s next opponents have conceded {prefersHome ? (100 - fixturesHome.length / allShotsAgainstFixtures.length * 100).toFixed(2) : (fixturesHome.length / allShotsAgainstFixtures.length * 100).toFixed(2)}% of their shots {prefersHome ? 'away from' : 'at'} home.
+    {player.team_title}'s next opponents have conceded {prefersHome
+        ? (100 - (fixturesHome.length / allShotsAgainstFixtures.length) * 100).toFixed(2)
+        : ((fixturesHome.length / allShotsAgainstFixtures.length) * 100).toFixed(2)}% of their shots
+    {prefersHome ? 'away from' : 'at'} home.
 </p>
