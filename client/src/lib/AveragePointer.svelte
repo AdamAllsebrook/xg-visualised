914de138103e02738621971de4057d10b0cd8531
Adam Allsebrook <adsorigin@gmail.com>
2023-04-04 18:16:57 +0100

    Show average xGA for fixtures

---
+++ client/src/lib/AveragePointer.svelte
@@ -0,0 +1,10 @@
+<script lang='ts'>
+    export let percentage: number;
+    export let text: string;
+</script>
+
+<span style='margin-left: {percentage}%' />
+<p class='inline-block -translate-x-1/2 text-white-800 pt-2'>{text}</p>
+<div />
+<span style='margin-left: {percentage}%' />
+<p class='inline-block -translate-x-1/2 text-white-800'>v</p>
--- client/src/lib/FixtureList.svelte
+++ client/src/lib/FixtureList.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { getContext } from 'svelte';
     import type { Writable } from 'svelte/store';
+    import AveragePointer from './AveragePointer.svelte';
     import { DataManager, key as dataKey } from './data/dataManager';
     import { key as shotsConcededKey, LeagueShotsConceded } from './data/leagueShotsConceded';
     import type { Opponents } from './data/opponents';
@@ -9,8 +10,14 @@
 
     const dataManager: Writable<DataManager> = getContext(dataKey);
     const opponents: Opponents = $dataManager.opponents;
-</script>
+    const leagueShotsConceded: Writable<LeagueShotsConceded | null> = getContext(shotsConcededKey);
+    // this is a constant redefined from another file, probably should fix that... 
+    const xgMax = 2.5;
 
+    $: $leagueShotsConceded, console.log($leagueShotsConceded?.data.values());
+    $: averageXGA = $leagueShotsConceded == null ? 0 : Array.from($leagueShotsConceded.data.entries()).reduce((a, [team, shots]) => a + shots.xG / $dataManager.teams.get(team).games, 0) / $leagueShotsConceded.data.size;
+</script>
+<AveragePointer percentage={averageXGA / xgMax * 100} text={`Average ${averageXGA.toFixed(2)} xGA per 90`} />
 {#each opponents.teams as team, i}
     <FixtureSummary
         {team}
