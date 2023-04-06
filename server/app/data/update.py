07a85e9d2f02b1f388ad600c3e0394fce9517b8c
Adam Allsebrook <adsorigin@gmail.com>
2023-04-06 18:28:57 +0100

    Fix cache update script

--- server/app/data/update.py
+++ server/app/data/update.py
@@ -1,12 +1,12 @@
 # Check if the cache needs updating every so often
 
 from arq import cron
-from app.redis_utils import reset_cache
+from typing import Any
 
-from app.data.year import *
-from app.data.players import *
-from app.data.teams import *
-from app.redis_utils import init_redis
+from app.data.year import get_year
+from app.data.players import get_all_players, get_player, get_player_matches, get_player_shots, get_all_shots_against_teams
+from app.data.teams import get_all_teams, get_team_fixtures
+from app.redis_utils import init_redis, reset_cache, is_cached
 
 
 init_redis()
@@ -14,29 +14,25 @@ init_redis()
 
 async def update_players(ctx: dict[Any, Any], *args: Any, **kwargs: Any) -> Any:
     players = await get_all_players()
+    year = await get_year()
 
-    for player in players:
-        await reset_cache(get_fpl_player, player.id)
-        cached_understat_player = await get_understat_player_seasons(player.id)
-        new_understat_player = reset_cache(get_understat_player_seasons, player.id)
-        if cached_understat_player != new_understat_player:
-            await reset_cache(get_understat_player_matches, player.id, year=None)
-            await reset_cache(get_understat_player_shots, player.id, year=None)
+    for id, player in players.items():
+        is_player_cached = await is_cached(get_player, id)
+        cached_understat_player = await get_player(id)
+        new_understat_player = await reset_cache(get_player, id)
+        if not is_player_cached or cached_understat_player != new_understat_player:
+            print(f'updating data for {player.player_name} ({"no cache existed" if not is_player_cached else "cache was out of date"})')
+            await reset_cache(get_player_matches, id, year=year)
+            await reset_cache(get_player_shots, id, year=year)
+
+    await get_all_shots_against_teams(year=year)
 
 
 async def update_teams(ctx: dict[Any, Any], *args: Any, **kwargs: Any):
-    teams = await get_all_teams()
-
-    do_update_matches = False
+    year = await get_year()
+    teams = await get_all_teams(year=year)
     for team in teams:
-        cached_team = await get_team(team.id)
-        new_team = await reset_cache(get_team, team.id)
-        if cached_team != new_team:
-            do_update_matches = True
-    if do_update_matches:
-        await reset_cache(get_all_fpl_matches)
-        for team in teams:
-            await reset_cache(get_fpl_team_matches, team.id)
+        await reset_cache(get_team_fixtures, team.title, year=year)
 
 
 async def update_year(ctx: dict[Any, Any], *args: Any, **kwargs: Any):
@@ -45,7 +41,7 @@ async def update_year(ctx: dict[Any, Any], *args: Any, **kwargs: Any):
 
 class WorkerSettings:
     cron_jobs = [
-        cron(update_year, minute={0, 30}),
-        cron(update_players, minute={0, 30}),
-        cron(update_teams, minute={0, 30}),
+        cron(update_year, minute=[0, 30]),
+        cron(update_players, minute=[0, 30]),
+        cron(update_teams, minute=[0, 30]),
     ]
--- server/app/redis_utils.py
+++ server/app/redis_utils.py
@@ -8,6 +8,12 @@ from typing import Callable, Any, ParamSpec, TypeVar, Awaitable
 P = ParamSpec('P')
 R = TypeVar('R')
 
+
+def generate_cache_key(func: Callable[P, Awaitable[R]], *args: P.args, **kwargs: P.kwargs) -> str:
+    cache_key = func.__name__ + ':' + ':'.join([str(v) for v in list(args) + list(kwargs.values())])
+    return cache_key
+
+
 def init_redis():
     global redis
     redis = aioredis.from_url("redis://localhost:6379")
@@ -16,7 +22,7 @@ def init_redis():
 def cache(func: Callable[P, Awaitable[R]]) -> Callable[P, Awaitable[R]]:
     @functools.wraps(func)
     async def wrapper(*args: P.args, **kwargs: P.kwargs):
-        cache_key = func.__name__ + ':' + ':'.join([str(v) for v in list(args) + list(kwargs.values())])
+        cache_key = generate_cache_key(func, *args, **kwargs)
         value = await redis.get(cache_key)
         if value is None:
             value = await func(*args, **kwargs)
@@ -30,6 +36,11 @@ def cache(func: Callable[P, Awaitable[R]]) -> Callable[P, Awaitable[R]]:
 
 
 async def reset_cache(func: Callable[P, Awaitable[R]], *args: P.args, **kwargs: P.kwargs) -> Awaitable[R]:
-    cache_key = func.__name__ + ':' + ':'.join([str(v) for v in list(args) + list(kwargs.values())])
+    cache_key = generate_cache_key(func, *args, **kwargs)
     await redis.delete(cache_key)
-    return func(*args, **kwargs)
+    return await func(*args, **kwargs)
+
+
+async def is_cached(func: Callable[P, Awaitable[R]], *args: P.args, **kwargs: P.kwargs) -> bool:
+    cache_key = generate_cache_key(func, *args, **kwargs)
+    return await redis.exists(cache_key)
