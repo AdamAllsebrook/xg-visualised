f5c20033a8ab94fd96c13d0833af075617ab7c4e
Adam Allsebrook <adsorigin@gmail.com>
2023-04-06 21:29:27 +0100

    Fix api url

--- client/src/client/core/OpenAPI.ts
+++ client/src/client/core/OpenAPI.ts
@@ -19,7 +19,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://xgvisualised.com',
+    BASE: 'https://api.xgvisualised.com',
     VERSION: '0.1.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
